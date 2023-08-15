abbc1c8a9efd601c722eb27c64110efc889f4788
Boini Aparna <79820411+aparnaboini@users.noreply.github.com>
2023-08-15 10:49:07 +0530

    Update PalindromeTest.java

--- src/test/java/com/demo/maven/PalindromeTest.java
+++ src/test/java/com/demo/maven/PalindromeTest.java
@@ -1,14 +1,14 @@
 package com.demo.maven;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class PalindromeTest {
+
 	@Test
 	public void whenEmptyString_thenAccept() {
 	    Palindrome palindromeTester = new Palindrome();
-	    assertTrue(palindromeTester.isPalindrome("olo"));
+	    assertTrue(palindromeTester.isPalindrome("madam"));
 	}
 }
